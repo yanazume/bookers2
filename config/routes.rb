08243170d84c53e5224a1b40d5a2c0a90b2f691b
yanazume <hideya1125@gmail.com>
2021-08-28 05:59:40 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
+
+  root to: 'homes#top'
+
+  get "/home/about" => "homes#about"
+  resources :users
+  resources :books
+end
